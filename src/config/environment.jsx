80abf746dff37c1b018e1e7908b9ab6ae9053891
Daniela Castillo <danielacastillogiron@gmail.com>
2025-02-08 16:30:45 -0600

    feat: add environment configuration

---
+++ src/config/environment.jsx
@@ -0,0 +1,8 @@
+const environment = {
+    production: false,
+    apiUrl: 'http://localhost:8180/api',
+    tokenName: 'authToken',
+    origin: 'http://localhost:4200'
+};
+
+export default environment;
